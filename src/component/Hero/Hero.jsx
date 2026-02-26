afe4791048a0794e569c42a1b532e382eaad484f
firoz671 <firozalmahmua671@gmail.com>
2026-02-26 12:42:08 +0600

    readme content deleted

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <>
-      <div className="w-10/12 h-125 mx-auto bg-[url(./src/assets/bg-shadow.png)] bg-cover bg-center flex items-center justify-center rounded-3xl mt-2 border-t-amber-500">
+      <div className="w-10/12 h-125 mx-auto bg-[url(../src/assets/bg-shadow.png)] bg-cover bg-center flex items-center justify-center rounded-3xl mt-2 border-t-amber-500">
         <div className=" text-center">
           <h1 className="text-3xl font-bold">
             Assemble Your Ultimate Dream 11 Cricket Team
